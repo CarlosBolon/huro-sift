72cf23d11b5c8c28c01044eeff5d89f9eef69bda
bkornel <bkornel@gmail.com>
2012-04-21 13:51:52 +0000

    nothing

--- Development/ObjectRecognition/ObjectRecognition/src/Main.cpp
+++ Development/ObjectRecognition/ObjectRecognition/src/Main.cpp
@@ -25,16 +25,24 @@ int main(int argc, char** argv)
 	auto_ptr<SiftFeature> thread1(new SiftFeature(1, frame));
 	auto_ptr<SiftFeature> thread2(new SiftFeature(2, frame));
 
+	//thread3 is created on the stack
+	SiftFeature thread3(3, frame);
+
 	thread1->Start();
 	thread2->Start();
+	thread3.Start();
 
 	// wait for the threads to finish
 	int result1 = reinterpret_cast<int>(thread1->Join());
 	int result2 = reinterpret_cast<int>(thread2->Join());
+	int result3 = reinterpret_cast<int>(thread3.Join());
 
-	cout << result1 << ' ' << result2 << ' ' <<  endl;
+	cout << result1 << ' ' << result2 << ' ' << result2 << endl;
 
-
+	imshow("first", thread1->frame_);
+	imshow("second", thread2->frame_);
+	imshow("third", thread3.frame_);
+	waitKey(0);
 	//std::auto_ptr<Runnable> r(new simpleRunnable(1));
 	//std::auto_ptr<Thread> thread0(new Thread(r));
 	//thread0->start();
