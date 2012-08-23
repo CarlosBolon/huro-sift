2b1eee5e57ff0874cd714e1e974c04f1b6464787
bkornel <bkornel@gmail.com>
2012-08-23 14:59:02 +0000

    runtime env.

--- Development/ObjectRecognition/resources/batch/Post_Build_Release.bat
+++ Development/ObjectRecognition/resources/batch/Post_Build_Release.bat
@@ -1,3 +1,4 @@
 xcopy "..\resources\opencv.2.4.2\bin\x86\dll\release" "..\Release" /E /Y
 xcopy "..\resources\tbb\release" "..\Release" /E /Y
 xcopy "..\resources\pthreads-win32\dll\pthreadVC2.dll" "..\Release" /E /Y
+xcopy "..\resources\vs.runtime\release" "..\Release" /E /Y
